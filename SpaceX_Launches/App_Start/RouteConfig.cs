76492d8d44ffec4328ac19c124716156314d9095
Sean McKinney <seanamckinney8@gmail.com>
2022-09-01 16:46:48 -0500

    switched the route to open to the page I want.

--- SpaceX_Launches/App_Start/RouteConfig.cs
+++ SpaceX_Launches/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace SpaceX_Launches
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Launches", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
